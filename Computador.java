b1e1bb2c9b2c93163118ce8445039b98c6ed8510
AntonioF2 <110260972+AntonioF2@users.noreply.github.com>
2022-08-29 16:55:27 -0300

    Lista 1 np1

---
+++ Computador.java
@@ -0,0 +1,25 @@
+public class Computador {
+
+    String marca;
+    float preco;
+
+
+    public Computador(String marca, float preço){
+
+    this.marca = marca;
+    this. preco = preco;
+    }
+
+    void mostraPCConfigs(){
+
+        System.out.println("Configurações do pc: " + );
+
+    }
+
+    void addMemoriaUSB(MemoriaUSB musb){
+
+    }
+
+
+
+}
