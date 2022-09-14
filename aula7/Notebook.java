fd821393c85ff28ecd99519db88c530eabb101cc
AntonioF2 <110260972+AntonioF2@users.noreply.github.com>
2022-09-14 20:23:39 -0300

    Add files via upload

---
+++ aula7/Notebook.java
@@ -0,0 +1,25 @@
+import static java.lang.String.valueOf;
+
+public class Notebook  extends Produto{
+
+    private double armazenamento;
+    private String gpu;
+    private String processador;
+
+
+
+    public Notebook(String nome, double preco, double armazenamento, String gpu, String processador) {
+        super(nome, preco);
+        this.armazenamento = armazenamento;
+        this.gpu = gpu;
+        this.processador = processador;
+    }
+
+    @Override
+    public String etiquetaPreco(){
+        return super.etiquetaPreco() + " Processador: " + this.processador + "GPU: " + this.gpu + "Armazenamento: " + this.armazenamento ;
+
+
+
+    }
+}
